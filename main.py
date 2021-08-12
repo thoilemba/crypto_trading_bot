049da4516458d275db1067b0ad605a123fb8ff5a
unknown <thoilembameetei.tm@gmail.com>
2021-08-12 23:44:07 +0530

    Upto lesson 25 completed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import logging
 from connectors.binance_futures import BinanceFutureClient
+from interface.root_component import Root
 
 logger = logging.getLogger()
 
@@ -24,5 +25,6 @@ if __name__ == '__main__':
     binance = BinanceFutureClient("dbc3a01149f0e22c4f038929cf6d02f13686616c5b038842492466a833f7e246",
                                   "8ad3cdc5e5045adb49db2fabf809008f75ee31d691dd4c86f0f05ce645c3d5e9", True)
 
-    root = tk.Tk()
+    root = Root()
     root.mainloop()
+
