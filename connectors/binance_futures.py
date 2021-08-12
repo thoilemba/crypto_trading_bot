57c240e79bd4160917cd54466c11ecf90b729c24
unknown <thoilembameetei.tm@gmail.com>
2021-08-12 13:01:58 +0530

    Lesson 13 completed

--- connectors/binance_futures.py
+++ connectors/binance_futures.py
@@ -1,24 +1,40 @@
 import logging
 import requests
+import time
+import hmac
+import hashlib
+from urllib.parse import urlencode
 
 
 logger = logging.getLogger()
 
 
 class BinanceFutureClient:
-    def __init__(self, testnet):
+    def __init__(self, public_key, secret_key, testnet):
         if testnet:
             self.base_url = "https://testnet.binancefuture.com"
         else:
             self.base_url = "https://fapi.binance.com"
 
+        self.public_key = public_key
+        self.secret_key = secret_key
+
+        self.headers = {'X-MBX-APIKEY': self.public_key}
+
         self.prices = dict()
 
         logger.info("Binance Futures Client successfully initialized")
 
+    def generate_signature(self, data):
+        return hmac.new(self.secret_key.encode(), urlencode(data).encode(), hashlib.sha256).hexdigest()
+
     def make_request(self, method, endpoint, data):
         if method == "GET":
-            response = requests.get(self.base_url + endpoint, params=data)
+            response = requests.get(self.base_url + endpoint, params=data, headers=self.headers)
+        elif method == "POST":
+            response = requests.post(self.base_url + endpoint, params=data, headers=self.headers)
+        elif method == "DELETE":
+            response = requests.delete(self.base_url + endpoint, params=data, headers=self.headers)
         else:
             raise ValueError()
 
@@ -52,7 +68,7 @@ class BinanceFutureClient:
 
         if raw_candles is not None:
             for c in raw_candles:
-                candles.append([c[0], float(c[1]), float(c[2]), float(c[3]),float(c[4]), float(c[5])])
+                candles.append([c[0], float(c[1]), float(c[2]), float(c[3]), float(c[4]), float(c[5])])
 
         return candles
 
@@ -70,5 +86,62 @@ class BinanceFutureClient:
 
         return self.prices[symbol]
 
+    def get_balances(self):
+        data = dict()
+        data['timestamp'] = int(time.time() * 1000)
+        data['signature'] = self.generate_signature(data)
 
+        balances = dict()
 
+        account_data = self.make_request("GET", "/fapi/v1/account", data)
+
+        if account_data is not None:
+            for a in account_data['assets']:
+                balances[a['asset']] = a
+
+        return balances
+
+    def place_order(self, symbol, side, quantity, order_type, price=None, tif=None):
+        data = dict()
+        data['symbol'] = symbol
+        data['side'] = side
+        data['quantity'] = quantity
+        data['type'] = order_type
+
+        if price is not None:
+            data['price'] = price
+
+        if tif is not None:
+            data['timeInForce'] = tif
+
+        data['timestamp'] = int(time.time() * 1000)
+        data['signature'] = self.generate_signature(data)
+
+        order_status = self.make_request("POST", "/fapi/v1/order", data)
+
+        return order_status
+
+    def cancel_order(self, symbol, order_id):
+
+        data = dict()
+        data['order_Id'] = order_id
+        data['symbol'] = symbol
+
+        data['timestamp'] = int(time.time() * 1000)
+        data['signature'] = self.generate_signature(data)
+
+        order_status = self.make_request("DELETE", "/fapi/v1/order", data)
+
+        return order_status
+
+    def get_order_status(self, symbol, order_id):
+
+        data = dict()
+        data['timestamp'] = int(time.time() * 1000)
+        data['symbol'] = symbol
+        data['order_id'] = order_id
+        data['signature'] = self.generate_signature(data)
+
+        order_status = self.make_request("GET", "/fapi/v1/order", data)
+
+        return order_status
--- main.py
+++ main.py
@@ -21,8 +21,8 @@ logger.addHandler(file_handler)
 
 if __name__ == '__main__':
 
-    binance = BinanceFutureClient(True)
-    print(binance.get_historical_candles("BTCUSDT", "1h"))
+    binance = BinanceFutureClient("dbc3a01149f0e22c4f038929cf6d02f13686616c5b038842492466a833f7e246",
+                                  "8ad3cdc5e5045adb49db2fabf809008f75ee31d691dd4c86f0f05ce645c3d5e9", True)
 
     root = tk.Tk()
     root.mainloop()
