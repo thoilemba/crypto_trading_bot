b14edf46a827a3a0547165319a39f9f3e1e9e2d7
unknown <thoilembameetei.tm@gmail.com>
2021-08-13 19:53:52 +0530

    Lesson 33 completed

--- interface/root_component.py
+++ interface/root_component.py
@@ -5,6 +5,7 @@ from connectors.binance_futures import BinanceFutureClient
 from interface.watchlist_component import Watchlist
 import logging
 from interface.trades_component import TradesWatch
+from interface.strategy_component import StrategyEditor
 
 logger = logging.getLogger()
 
@@ -30,6 +31,9 @@ class Root(tk.Tk):
 
         self._logging_frame = Logging(self._left_frame, bg=BG_COLOR)
         self._logging_frame.pack(side=tk.TOP)
+
+        self._strategy_frame = StrategyEditor(self._right_frame, bg=BG_COLOR)
+        self._strategy_frame.pack(side=tk.TOP)
 
         self._trades_frame = TradesWatch(self._right_frame, bg=BG_COLOR)
         self._trades_frame.pack(side=tk.TOP)
